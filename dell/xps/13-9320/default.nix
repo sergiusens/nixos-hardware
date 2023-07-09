9c159e9b230d9bd2e00c27b8f93781c0cea28203
Frederik Rietdijk <fridh@fridh.nl>
2023-07-09 12:23:14 +0200

    Dell XPS 13-9320
    Signed-off-by: Sergio Schvezov <sergio.schvezov@ubuntu.com>

---
+++ dell/xps/13-9320/default.nix
@@ -0,0 +1,11 @@
+{ lib, pkgs, ... }:
+{
+  imports = [
+    ../../../common/cpu/intel
+    ../../../common/pc/laptop
+    ../../../common/pc/ssd
+  ];
+
+  # Allows for updating firmware via `fwupdmgr`.
+  services.fwupd.enable = lib.mkDefault true;
+}
